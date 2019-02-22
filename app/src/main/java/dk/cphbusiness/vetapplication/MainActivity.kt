e6446a56e5e434c671078738951b94270c94ebbe
Anders Kalhauge <anders@kalhauge.dk>
2019-02-22 09:20:42 +0100

    gradle.build

--- app/src/main/java/dk/cphbusiness/vetapplication/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/vetapplication/MainActivity.kt
@@ -2,11 +2,14 @@ package dk.cphbusiness.vetapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        simple_list_button.onClick {  }
     }
 }
---
+++ app/src/main/java/dk/cphbusiness/vetapplication/SimpleActivity.kt
@@ -0,0 +1,12 @@
+package dk.cphbusiness.vetapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SimpleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simple)
+    }
+}
