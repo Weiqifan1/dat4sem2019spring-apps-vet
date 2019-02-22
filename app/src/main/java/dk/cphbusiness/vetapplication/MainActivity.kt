0def50d2db9fb9509a172d2d5bf4e48e4ea23a31
Anders Kalhauge <anders@kalhauge.dk>
2019-02-22 09:41:34 +0100

    list

--- app/src/main/java/dk/cphbusiness/vetapplication/MainActivity.kt
+++ app/src/main/java/dk/cphbusiness/vetapplication/MainActivity.kt
@@ -4,12 +4,13 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.sdk27.coroutines.onClick
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        simple_list_button.onClick {  }
+        simple_list_button.onClick { startActivity<SimpleActivity>() }
     }
 }
